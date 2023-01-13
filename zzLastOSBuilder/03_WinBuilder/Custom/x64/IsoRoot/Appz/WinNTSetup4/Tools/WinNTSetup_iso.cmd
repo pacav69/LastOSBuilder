ccb0eb7bb42e33a0fdf891a4501b8d4b31df6aab
PaulCav <pacav69@gmail.com>
2023-01-13 12:37:23 +1100

    upodate readme

--- zzLastOSBuilder/03_WinBuilder/Custom/x64/IsoRoot/Appz/WinNTSetup4/Tools/WinNTSetup_iso.cmd
+++ zzLastOSBuilder/03_WinBuilder/Custom/x64/IsoRoot/Appz/WinNTSetup4/Tools/WinNTSetup_iso.cmd
@@ -1,4 +1,7 @@
 @echo off
+rem Win11 script
+set "scriptver=0.0.1"
+title %~nx0  v%scriptver%
 
 IF %1!==! (
 echo.
--- zzLastOSBuilder/03_WinBuilder/Custom/x64/IsoRoot/ppAppsLive/zzzOptions_Disable_Search_Remove_Defender/Options.cmd
+++ zzLastOSBuilder/03_WinBuilder/Custom/x64/IsoRoot/ppAppsLive/zzzOptions_Disable_Search_Remove_Defender/Options.cmd
@@ -1,4 +1,8 @@
 @echo off
+rem Win11 script
+set "scriptver=0.0.1"
+title %~nx0  v%scriptver%
+
 md X:\$Options
 copy /Y %~d0\sources\$Options\Remove_Defender.exe X:\$Options
 copy /Y %~d0\sources\$Options\ToDo.cmd X:\$Options
