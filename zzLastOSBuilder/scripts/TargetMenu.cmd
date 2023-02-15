24ea942f6a7c1e062a1e9ced216c27ef52aba14d
PaulCav <pacav69@gmail.com>
2023-02-15 12:17:07 +1100

    new file:   zzLastOSBuilder/scripts/TargetMenu.cmd

---
+++ zzLastOSBuilder/scripts/TargetMenu.cmd
@@ -0,0 +1,263 @@
+TargetMenu.cmd
+@REM TargetMenu.cmd
+
+@REM HelpMenu.cmd
+@echo off
+rem Win11 script
+set scriptver=0.0.14
+title %~nx0  v%scriptver%
+
+set Debug=1
+
+@REM IF /I %debug% GTR 1 (
+
+@REM echo. ########################################
+@REM echo my project name is %ProjectName%
+@REM echo MCTool = %MCTool%
+@REM echo ISO = %ISO%
+@REM echo. ########################################
+@REM pause
+
+@REM ) 
+@REM ELSE 
+@REM (goto startcode)
+
+
+@REM :startcode
+
+@REM  call the "setvars.cmd" file in the Scripts directory
+call %CPS%\setvars.cmd
+echo.
+echo my project name is %ProjectName%
+@REM pause
+
+
+::-------------------------------------------------------------------------------------------
+:: LastOS Toolkit - TargetMenu 
+::-------------------------------------------------------------------------------------------
+:HelpMenu
+
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Target Menu
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+echo.
+echo.                             [A]   About
+echo.
+echo.                             [1]   Source
+echo.
+echo.                             [2]   Integrate
+echo.
+echo.                             [3]   Remove
+echo.
+echo.                             [4]   Customize
+echo.
+echo.                             [5]   Apply
+echo.
+echo.                             [6]   Target
+echo.
+echo.                             [7]   Tools
+echo.
+echo.                             [H]   Help
+echo.
+echo.
+echo.                             [X]   Quit
+echo.
+echo.===============================================================================
+echo.
+choice /C:A1234567HX /N /M "Enter Your Choice for help: "
+if errorlevel 10 goto :Quit
+if errorlevel 9 goto :HelpMenuHelp
+if errorlevel 8 goto :ToolsMenuHelp
+if errorlevel 7 goto :TargetMenuHelp
+if errorlevel 6 goto :ApplyMenuHelp
+if errorlevel 5 goto :CustomizeMenuHelp
+if errorlevel 4 goto :RemoveMenuHelp
+if errorlevel 3 goto :IntegrateMenuHelp
+if errorlevel 2 goto :SourceMenuHelp
+if errorlevel 1 goto :aboutHelp
+::-------------------------------------------------------------------------------------------
+
+
+
+ :aboutHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help About 
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo About describes the LastOSBuilder
+  echo. Checks for updates
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+:SourceMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help SourceMenu 
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the SourceMenu
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+
+:IntegrateMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help IntegrateMenu 
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the IntegrateMenu
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+
+
+:RemoveMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help RemoveMenu
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the RemoveMenuHelp
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+  
+:CustomizeMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help CustomizeMenu
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the CustomizeMenuHelp
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+ 
+:ApplyMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help ApplyMenu
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the ApplyMenuHelp
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+
+:TargetMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help TargetMenu
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the TargetMenuHelp
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::----------------------------------------------------------------------------------------
+
+ 
+:ToolsMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help ToolsMenu
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the ToolsMenuHelp
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+
+ 
+:HelpMenuHelp
+cls
+echo.===============================================================================
+echo.                           LastOS ToolKit Builder - Help HelpMenuHelp
+echo.                           v%BuilderVersion%                             .
+echo.===============================================================================
+ echo.
+  echo describes the HelpMenuHelp
+  echo. 
+  echo.
+  echo.
+  echo.
+  echo.===============================================================================
+
+  pause
+  goto :exitHelpMenu
+::-------------------------------------------------------------------------------------------
+
+
+@REM =================================
+:exitHelpMenu
+echo Returning to HelpMenu
+timeout 2 > NUL
+GOTO HelpMenu
+
+@REM =================================
+:Quit
--- zzLastOSBuilder/zzmenu.cmd
+++ zzLastOSBuilder/zzmenu.cmd
@@ -138,43 +138,6 @@ if errorlevel 1 goto :about
 ::-------------------------------------------------------------------------------------------
 
 
-
-@REM set INPUT=false
-@REM set "MENU_OPTION="
-@REM set "OPTION1_INPUT=" 
-@REM set "OPTION2_INPUT="
-@REM echo +===============================================+
-@REM echo . LastOS Builder - USER Main MENU               .
-@REM echo . v%BuilderVersion%                             .
-@REM echo +===============================================+
-@REM echo .                                               .
-@REM echo .  1) About                                     .
-@REM echo .  2) Rename First ISO                          .
-@REM echo .  3) Extract Source ISO                        .
-@REM echo .  4) hello                                     .
-@REM echo .  5) About                                     .
-@REM echo .  6) Rename_First_ISO                          .
-@REM echo .  7) Extract Source ISO                        .
-@REM echo .  8) Extract Source ISO                        .
-@REM echo .  9) Extract Source ISO                        .
-@REM echo .  U) Cleanup Utilities Menu                    .
-@REM echo .  x) EXIT                                      .
-@REM echo .                                               .
-@REM echo +===============================================+
-@REM set /P MENU_OPTION="OPTION: "
-
-@REM IF %MENU_OPTION%==1 GOTO OPTION1
-@REM IF %MENU_OPTION%==2 GOTO OPTION2
-@REM IF %MENU_OPTION%==3 GOTO OPTION3
-@REM IF %MENU_OPTION%==4 GOTO OPTION4
-@REM IF %MENU_OPTION%==5 GOTO OPTION5
-@REM IF %MENU_OPTION%==6 GOTO OPTION6
-@REM IF %MENU_OPTION%==7 GOTO OPTION7
-@REM IF %MENU_OPTION%==8 GOTO OPTION8
-@REM IF %MENU_OPTION%==9 GOTO OPTION9
-@REM IF %MENU_OPTION%==U GOTO OPTIONU
-@REM IF %MENU_OPTION%==x GOTO OPTION99
-@REM IF %INPUT%==false GOTO DEFAULT
 @REM =================================
 :about
 cls
@@ -223,7 +186,37 @@ GOTO MENU_START
 @REM =================================
 
 
+@REM =================================
+:ApplyMenu
+cls
+@REM echo errorlevel = %errorlevel%
+@REM pause
+call %CPS%\ApplyMenu.cmd
+rem timeout 2 > NUL
+GOTO MENU_START
+@REM =================================
 
+
+
+@REM =================================
+:TargetMenu
+cls
+@REM echo errorlevel = %errorlevel%
+@REM pause
+call %CPS%\TargetMenu.cmd
+rem timeout 2 > NUL
+GOTO MENU_START
+@REM =================================
+
+@REM =================================
+:ToolsMenu
+cls
+@REM echo errorlevel = %errorlevel%
+@REM pause
+call %CPS%\ToolsMenu.cmd
+rem timeout 2 > NUL
+GOTO MENU_START
+@REM =================================
 
 @REM =================================
 :HelpMenu
@@ -236,187 +229,12 @@ GOTO MENU_START
 @REM =================================
 
 
-
-
-
-
-
-
-
-:OPTION2
-@REM set INPUT=true
-@REM set /p OPTION2_INPUT="MESSAGE: "
-@REM echo MESSAGE: %OPTION2_INPUT%
-@REM timeout 2 > NUL
-cls
-call 00.0__Rename_First_ISO_To_Windows_Original_ISO.cmd
-IF %ERRORLEVEL% EQU 1 goto nofile
-GOTO MENU_START
-:nofile
-echo NO File exists
-pause
-GOTO MENU_START
-@REM =================================
-:OPTION3
-@REM set INPUT=true
-@REM set /p OPTION2_INPUT="MESSAGE: "
-@REM echo MESSAGE: %OPTION2_INPUT%
-@REM timeout 2 > NUL
-cls
-call 00.1_Extract_Source_ISO.cmd
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION4
-cls
-
-@REM rem This first for routine will give the current path without a trailing \
-@REM %~d0
-@REM cd "%~dp0"
-@REM cd %~dps0
-@REM for %%f in ("%CD%") do set CP=%%~sf
-@REM rem CPS= CP Scripts
-@REM set "CPS=%CP%\scripts"
-@REM rem User Set Variables:
-@REM @REM set /p ProjectName=<%CP%\Settings\ProjectName.txt
-@REM echo %CP%
-call %CPS%\hello.cmd
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION5
-cls
-call  %CPS%\00.0___About_.cmd
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION6
-cls
-call   %CPS%\00.0__Rename_First_ISO_To_Windows_Original_ISO
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION7
-cls
-call 00.1_Extract_Source_ISO.cmd
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION8
-cls
-call 00.1_Extract_Source_ISO.cmd
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION9
-cls
-call 00.1_Extract_Source_ISO.cmd
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTIONU
-cls
-call :Utilities
-pause
-echo finished
-timeout 2 > NUL
-GOTO MENU_START
-
-@REM =================================
-:OPTION99
-set INPUT=true
-echo Bye
-timeout 2 > NUL
-exit /b
-@REM =================================
-:DEFAULT
-echo Option not available
-timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-
-:Utilities
-SETLOCAL
-echo +===============================================+
-echo . LastOS Builder - Utilities MENU                    .
-echo . v%BuilderVersion%                             .
-echo +===============================================+
-echo .                                               .
-echo .  1) About                                     .
-echo .  2) Cleanout LastOSBuilder                        .
-echo .  3) Extract Source ISO                        .
-echo .  4) Extract Source ISO                        .
-echo .  5) Extract Source ISO                        .
-echo .  6) Extract Source ISO                        .
-echo .  7) Extract Source ISO                        .
-echo .  8) Extract Source ISO                        .
-echo .  9) Extract Source ISO                        .
-echo .  R) Return to Main Menu                    .
-echo .  x) EXIT                                      .
-echo .                                               .
-echo +===============================================+
-set /p MENU_OPTION="OPTION: "
-
-IF %MENU_OPTION%==1 GOTO OPTION1
-IF %MENU_OPTION%==2 GOTO OPTION2
-IF %MENU_OPTION%==3 GOTO OPTION3
-IF %MENU_OPTION%==4 GOTO OPTION4
-IF %MENU_OPTION%==5 GOTO OPTION5
-IF %MENU_OPTION%==6 GOTO OPTION6
-IF %MENU_OPTION%==7 GOTO OPTION7
-IF %MENU_OPTION%==8 GOTO OPTION8
-IF %MENU_OPTION%==9 GOTO OPTION9
-IF %MENU_OPTION%==R GOTO OPTIONR
-IF %MENU_OPTION%==x GOTO OPTION99
-IF %INPUT%==false GOTO DEFAULT
-@REM =================================
-@REM =================================
-:OPTION1
-@REM set INPUT=true
-@REM set /p OPTION1_INPUT="HOST: "
-@REM ping %OPTION1_INPUT%
-cls
-call 00.0___About_.cmd
-rem timeout 2 > NUL
-GOTO MENU_START
-@REM =================================
-:OPTION2
-cls
-call 00.01_Cleanout_LastOS_Builder.cmd
-rem timeout 2 > NUL
-GOTO MENU_START
-
-@REM =================================
-:OPTIONR
-echo Returning to Main Menu 
-timeout 2 > NUL
-GOTO MENU_START
-
 @REM =================================
 :Quit
 set INPUT=true
 echo Bye
 timeout 2 > NUL
 exit /b
-@REM =================================
-:DEFAULT
-echo Option not available
-timeout 2 > NUL
-GOTO MENU_START
 @REM =================================
 
 ENDLOCAL
